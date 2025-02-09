4029d226ca7f5c75acc095276fee057052b583dd
Nair <ciprian.balanica223@gmail.com>
2025-02-10 00:53:24 +0200

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
     // base: mode === "production" ? "/steam-wishlist/" : "/",
